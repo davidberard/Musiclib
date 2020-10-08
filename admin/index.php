18ac5875fb3c73f2c1b11fdab2abed870fd426f9
David Berard <webmaster@davidberard.net>
2020-10-08 16:27:02 -0400

    Bootstrap
    Removed BootStrap from the solution, added some very basic stuff to admin

---
+++ admin/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Music Lib Admin</title>
+</head>
+<body>
+
+<nav>
+	<ul>
+		<li><a href="#">All albums</a></li>
+		<li><a href="#">Genres</a></li>
+		<li><a href="#">Medias</a></li>
+		<li><span>---</span></li>
+		<li><a href="#">New album</a></li>
+	</ul>
+</nav>
+
+</body>
+</html>
